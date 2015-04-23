424a6dce3a2556924a6d7551d0f2b004f7069828
jdicasolo <jeremy.dicasolo@gmail.com>
2015-04-23 04:40:58 -0400

    added environment files

---
+++ config/env/test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+
+    db: "mongodb://heroku_app35525047:L3tsR@c3!@ds061371.mongolab.com:61371/herok_app35525047",
+    app: {
+        name: "how about a race"
+    }
+
+}
--- public/js/controllers/RaceCtrl.js
+++ public/js/controllers/RaceCtrl.js
@@ -15,14 +15,4 @@ angular.module('RaceCtrl', ['ui.grid'])
 		.success(function(data){
 			$scope.gridOptions.data = data;
 		});
-
-
-
-    //$scope.gridOptions.data = [
-    //{"name": "red moshannon", "sport": "wildwater", "location": "moshannon, pa", "date": "3/28/2015"},
-    //{"name": "elk river", "sport": "wildwater", "location": "elk, wv", "date": "4/28/2015"},
-    //{"name": "some bike race", "sport": "mountain bike", "location": "somewhere, pa", "date": "4/28/2015"},
-    //{"name": "bump jump and slide", "sport": "skiing", "location": "seven springs, pa", "date": "3/12/2015"}
-    //];
-
 });
