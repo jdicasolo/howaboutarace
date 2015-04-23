738b8eb6261c2c530c992fc0ce466332ac1f478b
jdicasolo <jeremy.dicasolo@gmail.com>
2015-04-23 05:28:29 -0400

    modified test connection string

--- config/env/test.js
+++ config/env/test.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-    db: "mongodb://heroku_app35525047:L3tsR@c3!@ds061371.mongolab.com:61371/herok_app35525047",
+    db: "mongodb://admin:L3tsR@c3!@ds061371.mongolab.com:61371/heroku_app35525047",
     app: {
         name: "how about a race"
     }
