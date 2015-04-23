180094c5f39e23e2bcfa346ab40bf7f297f402fb
jdicasolo <jeremy.dicasolo@gmail.com>
2015-04-23 05:48:58 -0400

    changed connection string

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
 module.exports = {
-    url : 'mongodb://localhost/howaboutarace'
+    url : 'mongodb://admin:L3tsR@c3!@ds061371.mongolab.com:61371/heroku_app35525047'
 }
+
+//mongodb://localhost/howaboutarace'
